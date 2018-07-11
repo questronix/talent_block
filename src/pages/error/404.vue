0fa44e1110167d93b8fa6336115e74aa583130f0
mhackyu <markpaderes0932@yahoo.com>
2018-07-11 09:24:14 +0800

    Modify error page layout

--- src/pages/error/404.vue
+++ src/pages/error/404.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1><strong>404</strong> Page not found.</h1>
+  <div class="four04">
+		<h1 class="h1-404">404</h1>
+		<p>Sorry! Page Not Found!</p>
+    <b-button href="/" variant="primary">Back to home</b-button>
+	</div>
 </template>
