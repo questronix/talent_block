a8fa61a65acf069efb6a576366313163bdfdaa96
vhid91 <vhid.abaiz@gmail.com>
2018-07-13 15:43:16 +0800

    Add transaction page Add setting page Modify 404 page Modify Login, signup pages

---
+++ src/pages/account/Settings.vue
@@ -0,0 +1,136 @@
+<template>
+  <base-layout>
+    <div slot="header">
+      <NavBar type="dashboard" heading="Account Settings" img="url('/static/img/bg2.png')"/>
+    </div>
+    <div slot="space"></div>
+    <div slot="body">
+	<AccountStats />
+	<div class="dashboard accountforms">
+		<b-container>
+			<b-row>
+				<b-col cols="3" class="basic-info">
+					<h5>Basic Information</h5>
+					<form>
+						Email
+						<br>
+						<input class="form-control" type="email" name="" placeholder="name@email.com">
+						<br>
+						Confirm Email
+						<br>
+						<input class="form-control" type="email" name="" placeholder="name@email.com">
+						<br>
+						Birthday
+						<br>
+						<input class="form-control" type="date" name="" placeholder="December 31, 1996">
+						<br>
+						Password
+						<br>
+						<input class="form-control" type="password" name="" placeholder="Password">
+						<br>
+						Confirm Password
+						<br>
+						<input class="form-control" type="password" name="" placeholder="Password">
+						<br>
+						Address
+						<br>
+						<input class="form-control" type="text" name="" placeholder="San Pedro Laguna">
+						<br>
+						Contact Number
+						<br>
+						<input class="form-control" type="text" name="" placeholder="+63-9xx-xxxx-xxx">
+						<br>
+						<input class="btn btn-primary" type="submit" name="" value="Update">
+					</form>
+				</b-col>
+
+				<div class="col card-info">
+					<div class="container">
+						<center>
+							<h5>Card Information</h5>
+							<button class="btn addcard">Add Card</button>
+						</center>
+						
+						<div class="row">
+
+							<div class="col">
+								
+								<br>
+								<div>
+									<p>Card Type: Master Card</p>
+									<p>Name: Jay Tacdoro</p>
+									<p>Card Number: 5321-xxxxxx-xxxx</p>
+								</div>
+							</div>
+							<div class="col-3">
+								
+								<br>
+								<p>Expiration Date: 12/2020</p>
+								<p>CVV: 260</p>
+								<button class="btn btn-primary editbtn">Edit</button>
+							</div>
+						</div>
+						<hr>
+						<div class="row">
+							<div class="col">
+								
+								<div>
+									<p>Card Type: Master Card</p>
+									<p>Name: Jay Tacdoro</p>
+									<p>Card Number: 5321-xxxxxx-xxxx</p>
+								</div>
+							</div>
+							<div class="col-3">
+								
+								<p>Expiration Date: 12/2020</p>
+								<p>CVV: 260</p>
+								<button class="btn btn-primary editbtn">Edit</button>
+							</div>
+						</div>
+						<hr>
+						<div class="row">
+							<div class="col">
+							
+								<div>
+									<p>Card Type: Master Card</p>
+									<p>Name: Jay Tacdoro</p>
+									<p>Card Number: 5321-xxxxxx-xxxx</p>
+								</div>
+							</div>
+							<div class="col-3">
+							
+								<p>Expiration Date: 12/2020</p>
+								<p>CVV: 260</p>
+								<button class="btn btn-primary editbtn">Edit</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</b-row>			
+		</b-container>
+	</div>
+	</div>
+  </base-layout>
+</template>
+
+<script>
+import BaseLayout from '../../layouts/BaseLayout.vue';
+import NavBar from '../../components/NavBar/NavBar.vue';
+import AccountStats from '../../components/AccountStats/AccountStats.vue';
+
+export default {
+  name: 'settings',
+  components: {
+    BaseLayout,
+	NavBar,
+	AccountStats,
+  }
+}
+</script>
+
+<style scoped>
+button.btn.btn-primary.editbtn {
+	float: right;
+	width: 100px;
+}
+</style>
---
+++ src/pages/account/TransactionLog.vue
@@ -0,0 +1,99 @@
+<template>
+  <base-layout>
+    <div slot="header">
+      <NavBar type="dashboard" heading="Dashboard" img="url('/static/img/bg2.png')"/>
+    </div>
+    <div slot="space"></div>
+    <div slot="body">
+	<AccountStats />
+	<div class="dashboard accountforms">
+		<div class="container">
+			<div class="padded-white">
+				<div class="completed-sub-detail" v-for="transaction in transactions" :key="transaction.id">
+					<h5>{{transaction.name}}</h5>
+                    <hr>
+					<div class="row">
+						<div class="col">
+							<p>Subject Code: {{transaction.subjectcode}}</p>
+							<p>Ref number: {{transaction.refnum}}</p>
+						</div>
+						<div class="col">
+							<p>School: {{transaction.school}}</p>
+							<p>Amount: <span>{{transaction.ammount}}</span></p>
+						</div>
+						<div class="col">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							    Option
+							  </button>
+							  <div class="dropdown-menu cat-dropdown">
+							  	<div class="btn-group">
+					
+							    <div class=""></div>
+							  </div>
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>	
+		</div>
+
+		
+	</div>
+	</div>
+  </base-layout>
+</template>
+
+<script>
+import BaseLayout from '../../layouts/BaseLayout.vue';
+import NavBar from '../../components/NavBar/NavBar.vue';
+import AccountStats from '../../components/AccountStats/AccountStats.vue';
+
+export default {
+  name: 'transactionlog',
+  components: {
+    BaseLayout,
+	NavBar,
+	AccountStats,
+  },
+  data() {
+    return {
+        transactions:
+        [
+            {
+                id: 1, name: 'Fundamental of UX/UI Design', subjectcode: 'FUX2016', refnum: '111231232', school: 'Ateneo De Manila University', ammount: '23,599'
+            },
+            {
+                id: 2, name: 'Responsive Website Basics: Code with HTML, CSS, and JavaScript', subjectcode: 'FGF2016', refnum: '12362232', school: 'Ateneo De Manila University', ammount: '33,000'
+            },
+            {
+                id: 3, name: 'Introduction to Meteor.js Development', subjectcode: 'IMD2016', refnum: '120482042', school: 'Ateneo De Manila University', ammount: '13,099'
+            },
+            {
+                id: 4, name: 'Web Application Development with JavaScript and MongoDB', subjectcode: 'MDB2018', refnum: '10275832', school: 'Ateneo De Manila University', ammount: '17,599'
+            }
+            ,{
+                id: 5, name: 'Responsive Website Tutorial and Examples', subjectcode: 'EXB2018', refnum: '193629372', school: 'Ateneo De Manila University', ammount: '19,599'
+            }
+        ]
+    };
+      
+  }
+}
+</script>
+
+<style scoped>
+.btn-group {
+    float: right;
+}
+
+.row{
+    padding: 20px;
+}
+
+p {
+    color: gray;
+}
+</style>
