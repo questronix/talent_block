de792e18640d6bfea44f544914a5ab52dfe1a0bd
noreenrojales <nrrojales@up.edu.ph>
2018-07-18 12:02:23 +0800

    [FRONTEND] Updated text fillers, added initial about page

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -6,9 +6,11 @@
         <div class="signupcontainer">
           <div class="signupform">
             <div class="singupcontent">
+              <h5><strong>CHOOSE. PAY. LEARN.</strong></h5>
               <span class="signup-span">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Suspendisse id venenatis nibh, a porttitor libero.
+                Create a <strong>Talent Block</strong> account to enroll to courses in 
+                just a few clicks and pay without waiting in line.
+                Already have a Talent Block account?<a class="link" href=#login>Login here.</a>
               </span>
               <br>
               <img src="/static/img/signupimg.svg" height="200px">
