4f5a8cfa158ab82260f569ab8e334ccd4868f514
mhackyu <markpaderes0932@yahoo.com>
2018-07-24 10:57:29 +0800

    Modify course list item display

--- src/components/Course/CourseListItem.vue
+++ src/components/Course/CourseListItem.vue
@@ -1,11 +1,11 @@
 <template>
   <li>
-    <img :src="course.imgUrl"><br>
-    <p>+ {{ course.count }} students</p>
-    <router-link :to="{ name: 'course' }">
-      <h5>{{ course.title}}</h5>
+    <img :src="course.thumbnail"><br>
+    <p>+ {{ course.slot }} students</p>
+    <router-link :to="{ path: 'courses/course?id=' + course.id }">
+      <h5>{{ course.name}}</h5>
     </router-link>
-    <p>{{course.desc}}</p>
+    <p>{{course.short_desc}}</p>
   </li>
 </template>
 
