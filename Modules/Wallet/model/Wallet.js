9f32fd57df0adfb16da54ef19dbace1d17f36fd8
mhackyu <markpaderes0932@yahoo.com>
2018-07-30 14:01:21 +0800

    Update current balance query

--- Modules/Wallet/model/Wallet.js
+++ Modules/Wallet/model/Wallet.js
@@ -7,11 +7,7 @@ module.exports.getBalance = (uid) => {
   const ACTION = '[getWallet]';
   logger.log('info', `${TAG}${ACTION}`);
   return new Promise((resolve, reject)=>{
-    db.execute(`
-      SELECT COALESCE(SUM(amount), 0) "wallet" 
-      FROM wallet 
-      WHERE user_id = ?
-      `, uid)
+    db.execute(`SELECT current_balance FROM wallet WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, uid)
       .then((data) => {
         resolve(data[0]);
       }).catch((err) => {
